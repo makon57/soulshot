a29d1b588fc70d46bebc6deae62d4587689852d8
Manna Kong <m.kong.57@gmail.com>
2021-07-15 11:49:25 -0500

    done with logout

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -24,4 +24,9 @@ router.post('/', asyncHandler(async(req, res, next) => {
   return res.json({ user });
 }));
 
+router.delete('/', (_req, res) => {
+  res.clearCookie('token');
+  return res.json({ message: 'success' });
+});
+
 module.exports = router;
