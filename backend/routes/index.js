1c42946d48dae76e4a3708d0a1b7230bc23676e1
Manna Kong <m.kong.57@gmail.com>
2021-07-15 15:19:29 -0500

    done with phase 0 of frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,31 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve('../frontend/build')));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+}
 
 module.exports = router;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
