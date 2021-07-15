d25af6d01ccae8f453e3ad06c6038f0ac70c873e
Manna Kong <m.kong.57@gmail.com>
2021-07-15 18:24:49 -0500

    fixed signup bug

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -19,7 +19,7 @@ function SignupFormPage() {
     e.preventDefault();
     if (password === confirmPassword) {
       setErrors([]);
-      return dispatch(sessionActions.login({ email, username, password }))
+      return dispatch(sessionActions.signup({ email, username, password }))
         .catch(async( res) => {
           const data = await res.json();
           if (data && data.errors)  setErrors(data.errors);
