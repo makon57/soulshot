7c2483faa6b0a3f7fe9354262972121d7e51a32e
Manna Kong <m.kong.57@gmail.com>
2021-07-15 10:58:27 -0500

    done with user model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
@@ -29,9 +30,62 @@ module.exports = (sequelize, DataTypes) => {
         len: [60,60]
       },
     },
-  }, {});
+  },
+  {
+    defaultScope: {
+      attributes: {
+        exclude: ['hasshedPassword', 'email', 'createdAt', 'updatedAt'],
+      },
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ['hasshedPassword'] },
+      },
+      loginUser: {
+        attributes: {},
+      },
+    },
+  });
   User.associate = function(models) {
     // associations can be defined here
   };
   return User;
+};
+
+User.prototype.toSafeObject = function() { // remember, this cannot be an arrow function
+  const { id, username, email } = this; // context will be the User instance
+  return { id, username, email };
+};
+
+User.prototype.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.hashedPassword.toString());
+};
+
+User.getCurrentUserById = async function (id) {
+  return await User.scope('currentUser').findByPk(id);
+};
+
+User.login = async function ({ credential, password }) {
+  const { Op } = require('sequelize');
+  const user = await User.scope('loginUser').findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+  if (user && user.validatePassword(password)) {
+    return await User.scope('currentUser').findByPk(user.id);
+  }
+};
+
+User.signup = async function ({ username, email, password }) {
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    username,
+    email,
+    hashedPassword,
+  });
+  return await User.scope('currentUser').findByPk(user.id);
 };
