fc438a754433d48666c2ba300784733a78413e28
Manna Kong <m.kong.57@gmail.com>
2021-07-15 11:24:24 -0500

    done with phase 3 user auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,21 +1,9 @@
 const router = require('express').Router();
-const { setTokenCookie } = require('../../utils/auth');
-const asyncHandler = require('express-async-handler');
-const { User } = require('../../db/models');
 
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
 
-router.get('/set-token-cookie', asyncHandler(async(req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    },
-  })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
 
 module.exports = router;
