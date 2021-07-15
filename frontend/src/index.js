ad63add7440ccd04ad8a0fab5bfbd2da0adb5a93
Manna Kong <m.kong.57@gmail.com>
2021-07-15 15:42:42 -0500

    done with login act & red

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 import App from './App';
 import configStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configStore();
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
