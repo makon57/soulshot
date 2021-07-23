6205ddeea1e5d1932707f69f2a11891f6ecc4e90
Manna Kong <m.kong.57@gmail.com>
2021-07-23 12:25:58 -0500

    collect and delete

--- frontend/src/components/AlbumList/index.js
+++ frontend/src/components/AlbumList/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ImageDetailModal from '../ImageDetailModal'
-import { listImages, deleteAlbumItem } from '../../store/home';
+import { listImages } from '../../store/home';
 import { deleteAlbum } from '../../store/albums';
 import '../../components/ImageList/ImageList.css';
 import { useHistory, useParams } from 'react-router-dom';
@@ -21,13 +21,13 @@ function AlbumImagesList() {
     history.push(`/`)
   }
 
-  const deleteItem = async (imageId, albumId) => {
-    const payload = {
-      imageId,
-      albumId
-    }
-    dispatch(deleteAlbumItem(payload, albumId));
-  }
+  // const deleteItem = async (imageId, albumId) => {
+  //   const payload = {
+  //     imageId,
+  //     albumId
+  //   }
+  //   dispatch(deleteAlbumItem(payload, albumId));
+  // }
 
   useEffect(() => {
     dispatch(listImages(id));
@@ -41,7 +41,7 @@ function AlbumImagesList() {
           <li key={image.id} className="item">
             <div className="image-container">
               <ImageDetailModal image={image} className="image"/>
-              <button onClick={() => deleteItem(image.id, id)}>Delete</button>
+              {/* <button onClick={() => deleteItem(image.id, id)}>Delete</button> */}
             </div>
           </li>
         ))}
--- frontend/src/components/ImageDetailModal/index.js
+++ frontend/src/components/ImageDetailModal/index.js
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import ImageDetail from './ImageDetail';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { deleteAlbumItem } from '../../store/home';
 import './ImageDetail.css';
+import AlbumsModal from '../AlbumsModal';
 
 function ImageDetailModal({ image }) {
-
+  const dispatch = useDispatch();
+  const { id } = useParams();
   const [showModal, setShowModal] = useState(false);
+
+  const deleteItem = async (imageId, albumId) => {
+    const payload = {
+      imageId,
+      albumId
+    }
+
+    dispatch(deleteAlbumItem(payload, albumId));
+  }
+
+  let content = null;
+
+  if (id) {
+    content =(
+      <button onClick={() => deleteItem(image.id, id)}>Delete</button>
+    )
+  } else {
+    content = (
+      <AlbumsModal image={image}/>
+    )
+  }
 
   return (
     <>
@@ -13,6 +39,7 @@ function ImageDetailModal({ image }) {
       <div className="middle">
         <h4 onClick={() => setShowModal(true)}>{image.title}</h4>
         <p onClick={() => setShowModal(true)}>{image.description}</p>
+        {content}
       </div>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
