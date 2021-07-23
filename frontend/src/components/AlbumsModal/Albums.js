72ff72dd3d0ee9c3b3aecb610406ae80f69cf1c6
Manna Kong <m.kong.57@gmail.com>
2021-07-22 22:42:02 -0500

    done with styling for create album

--- frontend/src/components/AlbumsModal/Albums.js
+++ frontend/src/components/AlbumsModal/Albums.js
@@ -1,26 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { Link } from 'react-router-dom';
 import { fetchAlbums, addToAlbumList } from '../../store/albums';
-import './AlbumsModal.css';
-import AlbumInput from './AlbumInput';
 
+import AlbumInput from './AlbumInput';
+import './AlbumsModal.css';
 
 function Albums({ image }) {
   const dispatch = useDispatch();
   let sessionUser = useSelector(state => state.session.user);
 
   const [showCreateAlbum, setShowCreateAlbum] = useState(false);
-  // const [imageId, setImageId] = useState(image.id);
-  // const [albumId, setAlbumId] = useState('');
-
 
   const albums = Object.values(useSelector((state) => state.album.albums));
   const sortedAlbums = albums.slice(0).reverse();
-
-  useEffect (() => {
-    setShowCreateAlbum(false);
-  }, [image.id]);
 
   let content = null;
   if (showCreateAlbum && sessionUser) {
@@ -35,31 +27,32 @@ function Albums({ image }) {
     }
   }, [dispatch, sessionUser]);
 
- const addImage = async (imageId, albumId) => {
+  useEffect(() => {
+    setShowCreateAlbum(false);
+  }, [image.id]);
+
+ const addImage = async (imageId, albumId, album) => {
 
     const payload = {
       imageId,
       albumId,
     }
-    console.log(payload);
 
     let addedImage = dispatch(addToAlbumList(payload, albumId));
-    if ({addedImage}) {
-      alert(`Image was added to ${addedImage.title} album.`)
+    if (addedImage) {
+      alert(`Image has been added to ${album.title} album.`)
     }
  }
 
   return (
     <div>
-      <div className="container">
-        <ul className="image-list">
-        {sortedAlbums.map((album) => (
-          <li key={album.id} className="albums">
-            <button onClick={() => addImage(image.id, album.id)}>{album.title}</button>
-          </li>
-        ))}
-        </ul>
-      </div>
+      <ul className="album-list">
+      {sortedAlbums.map((album) => (
+        <li key={album.id} className="albums">
+          <button onClick={() => addImage(image.id, album.id, album)}>{album.title}</button>
+        </li>
+      ))}
+      </ul>
       {content}
       <div>
         {(!showCreateAlbum) && (
