78254a3f5ce3c16d5e6549a99b14c8493f6a3297
Manna Kong <m.kong.57@gmail.com>
2021-10-10 22:48:49 -0500

    done with splash and redirects and polish

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -16,19 +16,6 @@ function SignupForm() {
 
   if (sessionUser) return <Redirect to="/" />;
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (password === confirmPassword) {
-  //     setErrors([]);
-  //     return dispatch(sessionActions.signup({ email, username, password }))
-  //       .catch(async( res) => {
-  //         const data = await res.json();
-  //         if (data && data.errors)  setErrors(data.errors);
-  //       });
-  //   }
-  //   return setErrors(['Confirm Password field must be the same as the Password field.']);
-  // };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password === confirmPassword) {
